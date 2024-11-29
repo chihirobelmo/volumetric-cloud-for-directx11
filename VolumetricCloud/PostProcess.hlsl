2aa2e0c84e723db144e7120c046d390f5b978854
chihirobelmo <chihirobelmo@gmail.com>
2024-11-29 23:55:27 +0900

    gausian blur

--- VolumetricCloud/PostProcess.hlsl
+++ VolumetricCloud/PostProcess.hlsl
@@ -19,6 +19,26 @@ VS_OUTPUT VS(float4 Pos : POSITION, float2 Tex : TEXCOORD0) {
     return output;
 }
 
+float4 GaussianBlur(float2 texCoord, float2 texelSize) {
+    float4 color = float4(0.0, 0.0, 0.0, 0.0);
+    float totalWeight = 0.0;
+
+    // Gaussian weights
+    float weights[5] = { 0.227027, 0.1945946, 0.1216216, 0.054054, 0.016216 };
+
+    // Sample the texture at multiple positions
+    for (int i = -1; i <= 1; ++i) {
+        for (int j = -1; j <= 1; ++j) {
+            float weight = weights[abs(i)] * weights[abs(j)] * 4.0;
+            color += cloudTexture.Sample(samplerState, texCoord + float2(i, j) * texelSize) * weight;
+            totalWeight += weight;
+        }
+    }
+
+    // Normalize the color
+    return color / totalWeight;
+}
+
 float4 PS(VS_OUTPUT input) : SV_TARGET {
     float4 primitiveColor = primitiveTexture.Sample(samplerState, input.Tex);
     float4 cloudColor = cloudTexture.Sample(samplerState, input.Tex);
@@ -27,7 +47,13 @@ float4 PS(VS_OUTPUT input) : SV_TARGET {
     float cloudDepthValue = cloudDepth.Sample(samplerState, input.Tex).r;
 
     float4 finalColor = skyBoxColor * (1.0 - primitiveColor.a) + primitiveColor;
-    finalColor = finalColor * (1.0 - cloudColor.a) + cloudColor;
+
+	uint dx, dy;
+	cloudTexture.GetDimensions(dx, dy);
+	float2 rcpro = rcp(float2(dx, dy));
+    float4 cloudColorSmoothed = GaussianBlur(input.Tex, rcpro);
+
+    finalColor = finalColor * (1.0 - cloudColorSmoothed.a) + cloudColorSmoothed;
 
     return finalColor;
 }
