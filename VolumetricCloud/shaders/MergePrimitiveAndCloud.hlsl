b7a465584739ccbd016ab241589863573407970d
chihirobelmo <chihirobelmo@gmail.com>
2025-02-02 17:36:42 +0900

    fixed BU gets all black when inside cloud

--- VolumetricCloud/shaders/MergePrimitiveAndCloud.hlsl
+++ VolumetricCloud/shaders/MergePrimitiveAndCloud.hlsl
@@ -33,86 +33,84 @@ VS_OUTPUT VS(float4 Pos : POSITION, float2 Tex : TEXCOORD0) {
 
 float4 BU(VS_OUTPUT input)
 {
-    const float2 g_ScreenParam = float2(3840.0, 2160.0); // TBD
-	const float2 kScreenSize = g_ScreenParam.xy * 2.0;
-	const float2 kScreenHalfSize = float2(512.0, 512.0); // TBD
-	const float4 kBilinearWeights[4] =
-	{
-		float4( 9.0/16.0, 3.0/16.0, 3.0/16.0, 1.0/16.0 ),
-		float4( 3.0/16.0, 9.0/16.0, 1.0/16.0, 3.0/16.0 ),
-		float4( 3.0/16.0, 1.0/16.0, 9.0/16.0, 3.0/16.0 ),
-		float4( 1.0/16.0, 3.0/16.0, 3.0/16.0, 9.0/16.0 )
-	};
+    const float2 g_ScreenParam = float2(1024.0, 1024.0); // TBD
+    const float2 kScreenSize = g_ScreenParam.xy * 2.0;
+    const float2 kScreenHalfSize = float2(512.0, 512.0); // TBD
+    const float4 kBilinearWeights[4] =
+    {
+        float4( 9.0/16.0, 3.0/16.0, 3.0/16.0, 1.0/16.0 ),
+        float4( 3.0/16.0, 9.0/16.0, 1.0/16.0, 3.0/16.0 ),
+        float4( 3.0/16.0, 1.0/16.0, 9.0/16.0, 3.0/16.0 ),
+        float4( 1.0/16.0, 3.0/16.0, 3.0/16.0, 9.0/16.0 )
+    };
 
-	int2 hiResUV = (int2)(input.Tex * kScreenSize + float2(0.1, 0.1));
-	int hiResIndex = (1 - (hiResUV.y & 0x01)) * 2 + (1 - (hiResUV.x & 0x01));
-	float4 hiResND = primitiveDepthTexture.Load( int3(hiResUV, 0), int2(0, 0) );
+    int2 hiResUV = (int2)(input.Tex * kScreenSize + float2(0.1, 0.1));
+    int hiResIndex = (1 - (hiResUV.y & 0x01)) * 2 + (1 - (hiResUV.x & 0x01));
+    float4 hiResND = primitiveDepthTexture.Sample(linearSampler, input.Tex);
 
-	int2 lowResUV = (int2)(input.Tex * kScreenHalfSize.xy + float2(0.1, 0.1));
-	float4 lowResND[4];
-	float4 lowResAO[4];
-	switch (hiResIndex)
-	{
-	case 0:
-		lowResND[0] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(0, 0) );
-		lowResND[1] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(1, 0) );
-		lowResND[2] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(0, 1) );
-		lowResND[3] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(1, 1) );
-		lowResAO[0] = cloudTexture.Load( int3(lowResUV, 0), int2(0, 0) );
-		lowResAO[1] = cloudTexture.Load( int3(lowResUV, 0), int2(1, 0) );
-		lowResAO[2] = cloudTexture.Load( int3(lowResUV, 0), int2(0, 1) );
-		lowResAO[3] = cloudTexture.Load( int3(lowResUV, 0), int2(1, 1) );
-		break;
-	case 1:
-		lowResND[0] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(-1, 0) );
-		lowResND[1] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(0, 0) );
-		lowResND[2] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(-1, 1) );
-		lowResND[3] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(0, 1) );
-		lowResAO[0] = cloudTexture.Load( int3(lowResUV, 0), int2(-1, 0) );
-		lowResAO[1] = cloudTexture.Load( int3(lowResUV, 0), int2(0, 0) );
-		lowResAO[2] = cloudTexture.Load( int3(lowResUV, 0), int2(-1, 1) );
-		lowResAO[3] = cloudTexture.Load( int3(lowResUV, 0), int2(0, 1) );
-		break;
-	case 2:
-		lowResND[0] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(0, -1) );
-		lowResND[1] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(1, -1) );
-		lowResND[2] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(0, 0) );
-		lowResND[3] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(1, 0) );
-		lowResAO[0] = cloudTexture.Load( int3(lowResUV, 0), int2(0, -1) );
-		lowResAO[1] = cloudTexture.Load( int3(lowResUV, 0), int2(1, -1) );
-		lowResAO[2] = cloudTexture.Load( int3(lowResUV, 0), int2(0, 0) );
-		lowResAO[3] = cloudTexture.Load( int3(lowResUV, 0), int2(1, 0) );
-		break;
-	case 3:
-		lowResND[0] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(-1, -1) );
-		lowResND[1] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(0, -1) );
-		lowResND[2] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(-1, 0) );
-		lowResND[3] = cloudDepthTexture.Load( int3(lowResUV, 0), int2(0, 0) );
-		lowResAO[0] = cloudTexture.Load( int3(lowResUV, 0), int2(-1, -1) );
-		lowResAO[1] = cloudTexture.Load( int3(lowResUV, 0), int2(0, -1) );
-		lowResAO[2] = cloudTexture.Load( int3(lowResUV, 0), int2(-1, 0) );
-		lowResAO[3] = cloudTexture.Load( int3(lowResUV, 0), int2(0, 0) );
-		break;
-	}
+    float2 lowResUV = (input.Tex * kScreenHalfSize.xy + float2(0.1, 0.1));
+    float4 lowResND[4];
+    float4 lowResCol[4];
+    switch (hiResIndex)
+    {
+    case 0:
+        lowResND[0] = cloudDepthTexture.Sample(linearSampler, input.Tex);
+        lowResND[1] = cloudDepthTexture.Sample(linearSampler, input.Tex + float2(1.0 / kScreenHalfSize.x, 0.0));
+        lowResND[2] = cloudDepthTexture.Sample(linearSampler, input.Tex + float2(0.0, 1.0 / kScreenHalfSize.y));
+        lowResND[3] = cloudDepthTexture.Sample(linearSampler, input.Tex + float2(1.0 / kScreenHalfSize.x, 1.0 / kScreenHalfSize.y));
+        lowResCol[0] = cloudTexture.Sample(linearSampler, input.Tex);
+        lowResCol[1] = cloudTexture.Sample(linearSampler, input.Tex + float2(1.0 / kScreenHalfSize.x, 0.0));
+        lowResCol[2] = cloudTexture.Sample(linearSampler, input.Tex + float2(0.0, 1.0 / kScreenHalfSize.y));
+        lowResCol[3] = cloudTexture.Sample(linearSampler, input.Tex + float2(1.0 / kScreenHalfSize.x, 1.0 / kScreenHalfSize.y));
+        break;
+    case 1:
+        lowResND[0] = cloudDepthTexture.Sample(linearSampler, input.Tex - float2(1.0 / kScreenHalfSize.x, 0.0));
+        lowResND[1] = cloudDepthTexture.Sample(linearSampler, input.Tex);
+        lowResND[2] = cloudDepthTexture.Sample(linearSampler, input.Tex - float2(1.0 / kScreenHalfSize.x, 1.0 / kScreenHalfSize.y));
+        lowResND[3] = cloudDepthTexture.Sample(linearSampler, input.Tex + float2(0.0, 1.0 / kScreenHalfSize.y));
+        lowResCol[0] = cloudTexture.Sample(linearSampler, input.Tex - float2(1.0 / kScreenHalfSize.x, 0.0));
+        lowResCol[1] = cloudTexture.Sample(linearSampler, input.Tex);
+        lowResCol[2] = cloudTexture.Sample(linearSampler, input.Tex - float2(1.0 / kScreenHalfSize.x, 1.0 / kScreenHalfSize.y));
+        lowResCol[3] = cloudTexture.Sample(linearSampler, input.Tex + float2(0.0, 1.0 / kScreenHalfSize.y));
+        break;
+    case 2:
+        lowResND[0] = cloudDepthTexture.Sample(linearSampler, input.Tex - float2(0.0, 1.0 / kScreenHalfSize.y));
+        lowResND[1] = cloudDepthTexture.Sample(linearSampler, input.Tex + float2(1.0 / kScreenHalfSize.x, -1.0 / kScreenHalfSize.y));
+        lowResND[2] = cloudDepthTexture.Sample(linearSampler, input.Tex);
+        lowResND[3] = cloudDepthTexture.Sample(linearSampler, input.Tex + float2(1.0 / kScreenHalfSize.x, 0.0));
+        lowResCol[0] = cloudTexture.Sample(linearSampler, input.Tex - float2(0.0, 1.0 / kScreenHalfSize.y));
+        lowResCol[1] = cloudTexture.Sample(linearSampler, input.Tex + float2(1.0 / kScreenHalfSize.x, -1.0 / kScreenHalfSize.y));
+        lowResCol[2] = cloudTexture.Sample(linearSampler, input.Tex);
+        lowResCol[3] = cloudTexture.Sample(linearSampler, input.Tex + float2(1.0 / kScreenHalfSize.x, 0.0));
+        break;
+    case 3:
+        lowResND[0] = cloudDepthTexture.Sample(linearSampler, input.Tex - float2(1.0 / kScreenHalfSize.x, 1.0 / kScreenHalfSize.y));
+        lowResND[1] = cloudDepthTexture.Sample(linearSampler, input.Tex - float2(0.0, 1.0 / kScreenHalfSize.y));
+        lowResND[2] = cloudDepthTexture.Sample(linearSampler, input.Tex - float2(1.0 / kScreenHalfSize.x, 0.0));
+        lowResND[3] = cloudDepthTexture.Sample(linearSampler, input.Tex);
+        lowResCol[0] = cloudTexture.Sample(linearSampler, input.Tex - float2(1.0 / kScreenHalfSize.x, 1.0 / kScreenHalfSize.y));
+        lowResCol[1] = cloudTexture.Sample(linearSampler, input.Tex - float2(0.0, 1.0 / kScreenHalfSize.y));
+        lowResCol[2] = cloudTexture.Sample(linearSampler, input.Tex - float2(1.0 / kScreenHalfSize.x, 0.0));
+        lowResCol[3] = cloudTexture.Sample(linearSampler, input.Tex);
+        break;
+    }
 
-	float totalWeight = 0.0;
-	float4 color = 0.0;
-	for( int i = 0; i < 4; ++i )
-	{
-		// float normalWeight = dot( lowResND[i].xyz, hiResND.xyz );
-		// normalWeight = pow( saturate(normalWeight), 32.0 );
+    float totalWeight = 0.0;
+    float4 color = 0.0;
+    for( int i = 0; i < 4; ++i )
+    {
+        float depthDiff = hiResND.r - lowResND[i].r;
+        float depthWeight = 1.0 / (0.0001 + abs(depthDiff)); // Add epsilon to avoid division by zero
 
-		float depthDiff = hiResND.w - lowResND[i].w;
-		float depthWeight = 1.0 / (1.0 + abs(depthDiff));
+		// inside cloud it gets all black so we need min value
+        float weight = 0.0001 + depthWeight * kBilinearWeights[hiResIndex][i];
+        totalWeight += weight;
+        color += lowResCol[i] * weight;
+    }
 
-		float weight = /*normalWeight * */depthWeight * kBilinearWeights[hiResIndex][i];
-		totalWeight += weight;
-		color += lowResAO[i] * weight;
-	}
+    color /= totalWeight;
 
-	color /= totalWeight;
-
-	return color;
+    return color;
 }
 
 float4 PS(VS_OUTPUT input) : SV_TARGET {
